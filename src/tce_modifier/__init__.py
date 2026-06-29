fcf455d8ff7283c0542cd625778c0bdd03521593
Jacob Jeffries <88559006+jwjeffr@users.noreply.github.com>
2026-06-29 15:53:37 +0000

    actually works in GUI now

--- src/tce_modifier/__init__.py
+++ src/tce_modifier/__init__.py
@@ -9,6 +9,7 @@ from itertools import permutations
 from ovito.data import DataCollection, DataTable
 from ovito.pipeline import ModifierInterface
 from ovito.io.ase import ovito_to_ase
+from ovito.traits import action_handler
 from traits.api import List, Float, Int, String
 from tce.calculator import TCECalculator
 from numpy.typing import NDArray
@@ -20,21 +21,31 @@ class TCEModifier(ModifierInterface):
 
     neighbor_cutoffs = List(Float, value=[3.0], label="Neighbor Cutoffs", minlen=1)
     many_body_features = List(List(Int), value=[], label="Many Body Features")
-    species = List(String, value=[], label="Species", minlen=1)
+    neighbor_tolerance = Float(value=0.01, label="Neighbor tolerance")
 
-    def modify(self, data: DataCollection, frame: int, **kwargs):
+    def modify(self, data: DataCollection, frame: int, input_slots, data_cache, **kwargs):
         
         atoms = ovito_to_ase(data)
 
-        if not hasattr(self, "calc"):
-            self.calc = TCECalculator(
+        if "calc" not in data_cache.attributes:
+            
+            trajectory_species = set()
+            for frame_index in range(input_slots["upstream"].num_frames):
+                frame_data = input_slots["upstream"].compute(frame_index)
+                frame_atoms = ovito_to_ase(frame_data)
+                trajectory_species |= set(frame_atoms.get_chemical_symbols())
+                
+            trajectory_species = sorted(trajectory_species)
+            
+            data_cache.attributes["calc"] = TCECalculator(
                 neighbor_cutoffs=np.array(self.neighbor_cutoffs),
                 many_body_features=self.many_body_features,
-                species=self.species
+                species=sorted(trajectory_species),
+                neighbor_tolerance=self.neighbor_tolerance
             )
 
-        cluster_vector = self.calc.get_feature_vector(atoms)
-        names = self.calc.get_feature_label_order()
+        cluster_vector = data_cache.attributes["calc"].get_feature_vector(atoms)
+        names = data_cache.attributes["calc"].get_feature_label_order()
 
         unique_clusters = defaultdict(float)
         for (topology, species), feature in zip(names, cluster_vector):
@@ -49,7 +60,7 @@ class TCEModifier(ModifierInterface):
         )
         table.x = table.create_property("Cluster Type", data=np.arange(len(unique_clusters)))
         for i, ((top, species), feature) in enumerate(unique_clusters.items()):
-            feature_label_str = f"{' & '.join(str(s) for s in top)}, {'-'.join(species)}"
+            feature_label_str = f"{' & '.join(str(s) for s in top)}\n{'-'.join(species)}"
             table.x.add_type_id(i, table, name=feature_label_str)
             data.attributes[feature_label_str] = feature
 
